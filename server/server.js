92d3479b66193bc27523b72e46bd4d0d9d2c6a46
Cynoc <cbb67@cornell.edu>
2018-04-08 00:24:31 -0500

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express =require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID}=require('mongodb');
 
 var {mongoose} =require('./db/mongoose');
 var {Todo} =require('./models/todo');
@@ -27,6 +27,20 @@ app.get('/todos',(req,res) =>{
   }, (e)=>{
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id=req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) =>{
+    if(todo){
+    res.send({todo});
+    }else{
+    return  res.status(400).send();
+    }
+  }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () =>{
